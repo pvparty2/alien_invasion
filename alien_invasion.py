ed4495854eb2762d4c92827a00d35a136ab307b6
pvparty <fake@example.com>
2021-03-01 18:57:03 -0600

    move ship left continuously

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,11 +34,13 @@ class AlienInvasion():
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-            
-
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- ship.py
+++ ship.py
@@ -23,6 +23,8 @@ class Ship:
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
