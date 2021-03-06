1b2200943f538944ec526750b5298303e3fe2d50
pvparty <fake@example.com>
2021-03-06 16:33:04 -0600

    make the fleet of aliens drop down when they hit the edges of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -128,7 +128,22 @@ class AlienInvasion():
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
+        """Check if fleet is at an edge, then update the positions fo all aliens in fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     # Make a game isntance, and run the game.
