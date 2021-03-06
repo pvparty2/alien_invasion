9a055dbcd5c556d917e4b4f71d98aaf114b13f20
pvparty <fake@example.com>
2021-03-06 16:24:22 -0600

    move the fleet of aliens

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Initialize alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load alien image and set its rect attributes.
         self.image = pygame.image.load('images/alien.bmp')
@@ -21,5 +22,8 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
 
-
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
 
